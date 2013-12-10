8e42c8297df4f62af57174e0ae45b7c85b1b6923
jefferson lara <jetox21@gmail.com>
2013-12-10 12:02:43 -0430

    Update vista_sistema.php

--- application/views/vista_sistema.php
+++ application/views/vista_sistema.php
@@ -2,7 +2,7 @@
 if ( ! defined('BASEPATH')) exit('No esta permitido el acceso directo');
 $primer_nombre = $info["nombre"];
 $base_url=base_url()."index.php/";
-echo 'siiiiiiiiiiiiiiiiiiiiiiiiiii';
+echo 'a verrrrrrrrrrrrrrr';
 ?>
 <script>
     $(function(){
