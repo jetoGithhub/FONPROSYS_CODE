f2529362932f9d92cc78cdf4165d2d1221504bdd
jetox <jetox@jetox-VIT-P2400>
2013-12-23 17:16:49 -0430

    Revert ""
    This reverts commit 1b04d24a259d4b18b51a10eef37dc92d75738939.

--- contribuyente.php
+++ contribuyente.php
@@ -1,4 +1,4 @@
 <?php
 $servidor = $_SERVER['SERVER_NAME'];
-header('location:http://'.$servidor.'/fonprosys_code/index.php/mod_contribuyente/inicio_c');
+header('location:http://'.$servidor.'/fonprocine_github/index.php/mod_contribuyente/inicio_c');
 ?>
